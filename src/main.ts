120448a54e7ccef456019276d425a30405ad8e31
Jan Haložan <j.halozan@gmail.com>
2017-01-23 21:10:26 +0100

    Stuff

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
 "use strict";
 
-import * as express from "express";
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+import * as path from "path";
 
 class Server {
+  public app: express.Application;
 
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  constructor() {
+    let app = express();
+
+    app.get('/', function(req: express.Request, res: express.Response) {
+      res.send("Derp");
+    });
+
+    this.app = app;
+    // this.config();
+
+
+  }
+
+  public start(port: number): void {
+    this.app.listen(port, function(){
+      console.log("Listening on " + port);
+    });
+  }
 }
+
+let server = Server.bootstrap();
+server.start(1337);
