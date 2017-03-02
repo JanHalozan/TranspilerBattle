5a2ee963c142733671e7eb45e642e70a2a4e28a7
Jan Haložan <j.halozan@gmail.com>
2017-03-02 19:25:32 +0100

    Babel simple hash

--- src/main.js
+++ src/main.js
@@ -8,6 +8,28 @@ app.get('/', function(req, res) {
   res.send("Hello world from Babel.");
 });
 
+app.get('/simple-hash', function(req, res) {
+  var hash = function(str) {
+    var s1 = 1;
+    var s2 = 0;
+
+    for (var i = 0; i < str.length; ++i) {
+      s1 = (s1 + str.charCodeAt(i)) % 65521;
+      s2 = (s1 + s2) % 65521;
+    }
+
+    return (s2 << 16) | s1;
+  };
+
+  if (req.query.message == null) {
+    res.status(400).send('Message not provided.');
+    return;
+  }
+
+  var val = hash(req.query.message);
+  res.send('{"val": ' + val.toString() + '}');
+});
+
 var port = process.env.port || 1337;
 app.listen(port, function() {
   console.log("Listening on " + port);
